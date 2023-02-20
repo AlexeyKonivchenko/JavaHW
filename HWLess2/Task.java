0f1b9979bc601be3a3fda262855848a0cef42893
AlexKon <konivchenko94@gmail.com>
2023-02-20 17:31:35 +0300

    решение задания

---
+++ HWLess2/Task.java
@@ -0,0 +1,39 @@
+package JavaHW.HWLess2;
+//Написать метод который приводит сторку к нормальному виду
+// строка на вход
+// Добрый    день  Как   дела Все    хорошо
+//выход
+// Добрый день. Как дела. Все хорошо.
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[J");
+        System.out.println("Была введена изначальная фраза: ");
+        String inputString = "Добрый    день  Как   дела Все    хорошо\n";
+        System.out.printf("" + inputString);
+        System.out.println("Фраза была пробразована в следующую: ");
+        System.out.printf("" + makeStringBetter(inputString));
+
+    }
+
+    public static String clearSpace(String inputString) {
+        inputString = inputString.replaceAll("\\s+", " ");
+        return inputString;
+    }
+
+    public static StringBuilder makeStringBetter(String string) {
+        string = clearSpace(string);
+        StringBuilder inputPhrase = new StringBuilder(string);
+        inputPhrase.insert(inputPhrase.length() - 1, '.');
+
+        for (int i = 1; i < inputPhrase.length(); i++) {
+
+            if (Character.isUpperCase(inputPhrase.charAt(i))) {
+                inputPhrase.insert(i - 1, '.');
+                i++;
+            }
+        }
+
+        return inputPhrase;
+    }
+}
